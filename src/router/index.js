32600a8977c897a5d7fe024219135039c966119b
FanXueJing-fan <2209221124@qq.com>
2020-11-09 15:34:25 +0800

    创建vue项目，搭建路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,54 @@
+// 引入所有的组件名大写
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
+import Classify from '../views/Classify/index'
+import Hello from '../views/Hello'
+import Home from '../views/Home'
+import History from '../views/Hello/History.vue'
+import Favorite from '../views/Hello/Favorite.vue'
+import Login from '../views/Login'
+import My from '../views/My'
+import Ranking from '../views/Ranking'
+import Register from '../views/Register'
+import Search from '../views/Search'
+import SearchResult from '../views/SearchResult'
+import Vip from '../views/Vip'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
+// 路由对象
 const router = new VueRouter({
-  routes
+  // 路由规则
+  routes: [
+    { path: '/', redirect: '/home' },
+    { path: '/classify', component: Classify },
+    {
+      path: '/hello',
+      component: Hello,
+      children: [
+        {
+          path: 'history',
+          component: History
+        },
+        {
+          path: 'favorite',
+          component: Favorite
+        },
+        {
+          path: '',
+          redirect: '/hello/favorite'
+        }
+      ]
+    },
+    { path: '/home', component: Home },
+    { path: '/login', component: Login },
+    { path: '/my', component: My },
+    { path: '/ranking', component: Ranking },
+    { path: '/register', component: Register },
+    { path: '/search', component: Search },
+    { path: '/searchResult', component: SearchResult },
+    { path: '/vip', component: Vip }
+  ]
 })
-
 export default router
