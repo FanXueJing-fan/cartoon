be0824e56c60b2410c056b2862a6bb6f8526f9de
FanXueJing-fan <2209221124@qq.com>
2020-11-09 22:44:44 +0800

    在已经写好的swiper上重新实现swiper

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -76,9 +76,46 @@ export default {
       background-size: 100%;
     }
   }
-  .swiper-container{
+   .swiper-container{
     width: 100%;
     height: 170px;
+    .swiper-pagination-bullet {
+      opacity: 1;
+      vertical-align: middle;
+      width: 6px;
+      height: 6px;
+      border-radius: 50%;
+      background-color: hsla(0, 0%, 100%, 0.7);
+    }
+
+    .swiper-pagination-bullet-active {
+      width: 20px;
+      height: 10px;
+      background: url("../../assets/icon/dot.png") no-repeat;
+      background-size: 100%;
+    }
   }
 }
+</style>
+//  我现在想要重新写小圆点，引入swiper，浏览器就会加载出小圆点的样式，不能放在scoped(只会合并或者是权重相同没有效果)，想要实现全局改变，就要放在全局，而不是局部
+<style lang="scss">
+   .swiper-container{
+    width: 100%;
+    height: 170px;
+    .swiper-pagination-bullet {
+      opacity: 1;
+      vertical-align: middle;
+      width: 6px;
+      height: 6px;
+      border-radius: 50%;
+      background-color: hsla(0, 0%, 100%, 0.7);
+    }
+
+    .swiper-pagination-bullet-active {
+      width: 20px;
+      height: 10px;
+      background: url("../../assets/icon/dot.png") no-repeat;
+      background-size: 100%;
+    }
+  }
 </style>
